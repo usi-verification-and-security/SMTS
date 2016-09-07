5fe124601625f9c73322fc5cf79e8b17b170d4a4
Matteo <matteo@tilde.mobile.usilu.net>
2016-09-07 17:05:59 +0200

    partitioning for opensmt partially working

---
+++ src/client/OpenSMT2/SplitProcess.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Matteo on 07/09/16.
+//
+
+#include "SplitProcess.h"
---
+++ src/client/OpenSMT2/SplitProcess.h
@@ -0,0 +1,14 @@
+//
+// Created by Matteo on 07/09/16.
+//
+
+#ifndef CLAUSE_SERVER_SPLITPROCESS_H
+#define CLAUSE_SERVER_SPLITPROCESS_H
+
+
+class SplitProcess {
+
+};
+
+
+#endif //CLAUSE_SERVER_SPLITPROCESS_H
