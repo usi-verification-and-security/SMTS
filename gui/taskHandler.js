c07b11a788a82bef47c65f9deb3886d4af5d05cd
Amedeo Zucchetti <amedeo.zucchetti@gmail.com>
2017-07-18 11:36:53 +0200

    Refactor `taskHandler.js`

--- gui/taskHandler.js
+++ gui/taskHandler.js
@@ -1,58 +1,58 @@
 let exec = require('child_process').execSync;
 let config = require('./config.js');
 
-let path = config.path;     // Path to the server python file
-let port = config.port;     // Server port
-let python = config.python; // Python command-line
-
-function run(command) {
-    if (!port) {
-        console.log('Error: no port');
-        process.exit();
-    } else if (!path) {
-        console.log('Error: no path');
-        process.exit();
-    }
-    return exec(`${command} | ${python} ${path} ${port}`, {'encoding': 'utf8'}).trim();
+function run(command, args = '') {
+    return exec(`echo '${command}' | ${config.python} ${config.client} ${config.port} ${args}`, {'encoding': 'utf8'}).trim();
 }
 
 module.exports = {
 
+    newInstance: function(filename) {
+        return run(``, filename);
+    },
+
     setPath: function (_path) {
-        path = _path;
+        config.client = _path;
     },
 
     setPort: function (_port) {
-        port = _port;
+        config.port = _port;
+    },
+
+    getVersion: function() {
+        return parseInt(run(``, `--version`));
     },
 
     getDatabase: function () {
-        return run("echo 'self.config.db().execute(\"PRAGMA database_list\").fetchall()[0][2] if self.config.db() else \"Empty\"'");
+        return run(`self.config.db().execute("PRAGMA database_list").fetchall()[0][2] if self.config.db() else "Empty"`);
     },
 
     getInstance: function () {
-        return run("echo 'self.current.root.name if self.current else \"Empty\"'");
+        return run(`self.current.root.name if self.current else "Empty"`);
+    },
+
+    getTimeout: function() {
+        return run(`self.current.timeout`);
+    },
+
+    getElapsedTime: function() {
+        return run(`time.time() - self.current.when_started`);
     },
 
     getRemainingTime: function () {
-        return run("echo 'self.current.when_timeout if self.current else \"Empty\"'");
+        return run(`self.current.when_timeout if self.current else "Empty"`);
     },
 
     getCurrent: function () {
         return [this.getInstance(), this.getRemainingTime()];
     },
 
-    increaseTimeout: function (timeout) {
-        return run(`echo 'exec('self.current.timeout+='${timeout}')' if self.current != 'None' else \"Nothing to reset\"'`);
-
-    },
-
-    decreaseTimeout: function (timeout) {
-        return run(`echo 'exec('self.current.timeout-='${timeout}')' if self.current != 'None' else \"Nothing to reset\"'`);
+    changeTimeout: function (delta) {
+        return run(`exec("self.current.timeout+=${delta}") if self.current != None else "Nothing to reset"`);
     },
 
     stopSolving: function () {
-        return run("echo 'exec('self.current.timeout=1')' if self.current != 'None' else \"Nothing to reset\"'");
+        return run(`exec("self.current.timeout=1") if self.current != None else "Nothing to reset"`);
     }
 
 };
