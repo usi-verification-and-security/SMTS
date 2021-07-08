64d4795990ba8624a2bb01dc2ac81dc588e8dc10
Masoud Asadzadeh <masoud.asadzade@gmail.com>
2021-07-08 02:09:51 +0200

    Update config

--- awcCloudTrack/awsRunBatch/mpi-run.sh
+++ awcCloudTrack/awsRunBatch/mpi-run.sh
@@ -69,7 +69,8 @@ wait_for_nodes () {
    # fi
   #done
   echo "Send .smt2 Instance"
-  SMTS/server/client.py 3000 test.smt2
+  SMTS/server/client.py 3000 SMTS/hpcClusterBenchs-timedout/${COMP_S3_PROBLEM_PATH}
+#  SMTS/awcCloudTrack/awsRunBatch/run_aws_smtsClient.sh "SMTS/hpcClusterBenchs-timedout"
   wait
 }
 
@@ -88,7 +89,7 @@ report_to_master () {
     echo "Sleeping 1 seconds and trying again"
   done
   #echo "$ip slots=2" >> $HOST_FILE_PATH${AWS_BATCH_JOB_NODE_INDEX}
-  mpirun --mca btl_tcp_if_include eth0 --allow-run-as-root -np 4  --hostfile $HOST_FILE_PATH${AWS_BATCH_JOB_NODE_INDEX} SMTS/build/solver_opensmt -s ${AWS_BATCH_JOB_MAIN_NODE_PRIVATE_IPV4_ADDRESS}:3000 &
+  mpirun --mca btl_tcp_if_include eth0 --allow-run-as-root -np 8  --hostfile $HOST_FILE_PATH${AWS_BATCH_JOB_NODE_INDEX} SMTS/build/solver_opensmt -s ${AWS_BATCH_JOB_MAIN_NODE_PRIVATE_IPV4_ADDRESS}:3000 &
 
   ps -ef | grep sshd
   wait
