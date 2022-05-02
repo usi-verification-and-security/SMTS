b26d2f81d9c35f5227732bd4144d42b2d663dd02
Masoud Asadzadeh <masoud.asadzade@gmail.com>
2022-05-02 22:31:56 +0200

    [ci skip] lemma server's driver: adapt to PTPLib

--- src/lemma_server/main.cpp
+++ src/lemma_server/main.cpp
@@ -1,22 +1,31 @@
+/*
+ * Copyright (c) Matteo Marescotti <Matteo.marescotti@usi.ch>
+ * Copyright (c) 2022, Antti Hyvarinen <antti.hyvarinen@gmail.com>
+ * Copyright (c) 2022, Seyedmasoud Asadzadeh <seyedmasoud.asadzadeh@usi.ch>
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#include <PTPLib/common/Exception.hpp>
+
 #include <iostream>
 #include "lib/Logger.h"
 #include "Settings.h"
 #include "LemmaServer.h"
-
 
 int main(int argc, char **argv) {
     Settings settings = Settings();
     try {
         settings.load(argc, argv);
     }
-    catch (Exception &ex) {
+    catch (PTPLib::common::Exception &ex) {
         Logger::log(Logger::ERROR, ex.what());
     }
 
     try {
         LemmaServer server(settings.port, settings.server, settings.db_filename, settings.send_again);
         server.run_forever();
-    } catch (Exception &ex) {
+    } catch (PTPLib::common::Exception &ex) {
         Logger::log(Logger::ERROR, ex.what());
     }
 }
