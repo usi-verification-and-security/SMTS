b3329ae460a528ea3d6d1f82fe7251b877860d22
Masoud Asadzadeh <masoud.asadzade@gmail.com>
2022-05-23 19:07:52 +0200

    make_smts: Refactor to be used by both ci system and docker and define an ev for parallel library of openSMT

--- bin/make_smts.sh
+++ bin/make_smts.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
 set -ev
+if [ -d build ]; then rm -rf build; fi
+mkdir -p build
 cd build
-cmake -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} -DCMAKE_CXX_FLAGS="$FLAGS" -DUSE_READLINE:BOOL=${USE_READLINE} ..
+
+cmake -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} \
+      -DCMAKE_CXX_FLAGS="${FLAGS}" \
+      -DUSE_READLINE:BOOL=${USE_READLINE} \
+      -DENABLE_LINE_EDITING:BOOL=${ENABLE_LINE_EDITING} \
+      -DPARALLEL:BOOL=${PARALLEL} \
+      ..
+
 make -j4
