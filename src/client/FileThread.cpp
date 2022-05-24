c9e282a312d6d8c0c3495806ea296ff24ce956e0
Masoud Asadzadeh <masoud.asadzade@gmail.com>
2022-05-24 19:28:08 +0200

    FileThread: Rafactor to adapt PTPLib and some improvment

--- src/client/FileThread.cpp
+++ src/client/FileThread.cpp
@@ -1,30 +1,35 @@
-//
-// Author: Matteo Marescotti
-//
+/*
+ * Copyright (c) Matteo Marescotti <Matteo.marescotti@usi.ch>
+ * Copyright (c) 2022, Antti Hyvarinen <antti.hyvarinen@gmail.com>
+ * Copyright (c) 2022, Seyedmasoud Asadzadeh <seyedmasoud.asadzadeh@usi.ch>
+ *
+ * SPDX-License-Identifier: MIT
+ */
 
-#include <iostream>
-#include <fstream>
+#include "FileThread.h"
 #include "lib/Logger.h"
 #include "lib/Process.h"
 #include "lib/Thread.h"
 #include "lib/sqlite3.h"
 #include "lib/net.h"
-#include "lib/memory.h"
-#include "lib/lib.h"
-#include "FileThread.h"
 
+#include <PTPLib/common/Memory.hpp>
+#include <PTPLib/common/Lib.hpp>
+
+#include <iostream>
+#include <fstream>
 
 FileThread::FileThread(Settings &settings) :
         settings(settings),
         server((uint16_t) 0) {
     if (settings.server.size())
-        throw Exception(__FILE__, __LINE__, "server must be null");
+        throw PTPLib::common::Exception(__FILE__, __LINE__, "server must be null");
     settings.server = to_string(this->server.get_local());
     this->main();
 }
 
 void FileThread::main() {
-    net::Header header;
+    PTPLib::net::Header header;
 
     std::shared_ptr<net::Socket> client(this->server.accept());
     std::shared_ptr<net::Socket> lemmas;
@@ -34,9 +39,10 @@ void FileThread::main() {
             lemmas.reset(new net::Socket(this->settings.lemmas));
         } catch (net::SocketException) {
         }
-        header["command"] = "lemmas";
-        header["lemmas"] = this->settings.lemmas;
-        client->write(header, "");
+        header[PTPLib::common::Param.COMMAND] = PTPLib::common::Command.LEMMAS;
+        header[PTPLib::common::Command.LEMMAS] = this->settings.lemmas;
+
+        client->write(PTPLib::net::SMTS_Event(header));
     }
 
     for (auto &filename : this->settings.files) {
@@ -55,44 +61,45 @@ void FileThread::main() {
 
         header.clear();
         for (auto &it : this->settings.parameters) {
-            header.set(net::Header::parameter, it.first, it.second);
+            header.set(PTPLib::net::parameter, it.first, it.second);
         }
-        header["command"] = "solve";
-        header["name"] = filename;
-        header["node"] = "[]";
-        client->write(header, payload);
+        header[PTPLib::common::Param.COMMAND] = PTPLib::common::Command.SOLVE;
+        header[PTPLib::common::Param.NAME] = filename;
+        header[PTPLib::common::Param.NODE] = "[]";
+        client->write(PTPLib::net::SMTS_Event(std::move(header), std::move(payload)));
         if (this->settings.dump_clauses) {
-            header["command"] = "cnf-clauses";
-            client->write(header, payload);
+            header[PTPLib::common::Param.COMMAND] = PTPLib::common::Command.CNFCLAUSES;
+            client->write(PTPLib::net::SMTS_Event(std::move(header), std::move(payload)));
         }
         do {
-            client->read(header, payload);
-            if (header["report"] == "cnf-clauses") {
+            uint32_t length = 0;
+            auto SMTS_event = client->read(length);
+            if (SMTS_event.header[PTPLib::common::Param.REPORT] == PTPLib::common::Command.CNFCLAUSES) {
                 auto clauses_filename = filename + ".cnf";
                 std::ofstream clauses_file(clauses_filename);
-                clauses_file << payload << "\n";
+                clauses_file << SMTS_event.body << "\n";
                 Logger::log(Logger::INFO, "clauses stored in " + clauses_filename);
                 break;
             }
             if (this->settings.verbose) {
                 Logger::log(Logger::INFO, header);
             }
-            if (header["report"] == "sat" || header["report"] == "unsat" || header["report"] == "unknown") {
+            if (header[PTPLib::common::Param.REPORT] == "sat" || header[PTPLib::common::Param.REPORT] == "unsat" || header[PTPLib::common::Param.REPORT] == "unknown") {
                 if (!this->settings.dump_clauses)
                     break;
             }
         } while (true);
         if (lemmas and !this->settings.keep_lemmas) {
             try {
-                header["lemmas"] = "0";
-                lemmas->write(header, "");
+                header[PTPLib::common::Command.LEMMAS] = "0";
+                lemmas->write(PTPLib::net::SMTS_Event(std::move(header)));
             } catch (net::SocketException) {
                 lemmas.reset();
             }
         }
-        header["command"] = "stop";
-        header["name"] = filename;
-        header["node"] = "[]";
-        client->write(header, "");
+        header[PTPLib::common::Param.COMMAND] = PTPLib::common::Command.STOP;
+        header[PTPLib::common::Param.NAME] = filename;
+        header[PTPLib::common::Param.NODE] = "[]";
+        client->write(PTPLib::net::SMTS_Event(std::move(header)));
     }
 }
--- src/client/FileThread.h
+++ src/client/FileThread.h
@@ -1,6 +1,10 @@
-//
-// Author: Matteo Marescotti
-//
+/*
+ * Copyright (c) Matteo Marescotti <Matteo.marescotti@usi.ch>
+ * Copyright (c) 2022, Antti Hyvarinen <antti.hyvarinen@gmail.com>
+ * Copyright (c) 2022, Seyedmasoud Asadzadeh <seyedmasoud.asadzadeh@usi.ch>
+ *
+ * SPDX-License-Identifier: MIT
+ */
 
 #ifndef SMTS_CLIENT_FILETHREAD_H
 #define SMTS_CLIENT_FILETHREAD_H
@@ -21,6 +25,5 @@ public:
     FileThread(Settings &);
 
 };
-
 
 #endif
