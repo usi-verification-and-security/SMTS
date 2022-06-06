399b896fc100d674ca1788d12eac96aa09285907
Masoud Asadzadeh <masoud.asadzade@gmail.com>
2022-06-06 19:40:39 +0200

    run-server-client: refactor to send bunch of instances

--- bin/run-server-client.sh
+++ bin/run-server-client.sh
@@ -1,17 +1,7 @@
 #!/bin/bash
 
-OPENSMTDIR="build/_deps/opensmt-src"
-./server/smts.py -d test.db -o4 -l  &
-sleep 3
-./server/client.py 3000  ${OPENSMTDIR}/regression/QF_UF/NEQ004_size4.smt2
-sleep 3
-./server/client.py 3000  ${OPENSMTDIR}/regression/QF_LIA/can_solve/ex10100_2600_100.smt2
-sleep 3
-./server/client.py 3000  ${OPENSMTDIR}/regression/QF_UF/php_3_3_40_sat.smt2
-sleep 3
-./server/client.py 3000  ${OPENSMTDIR}/regression/QF_UF/php_3_3_40_unsat.smt2
-sleep 3
-./server/client.py 3000  -t
+OPENSMTDIR=$(pwd)"/build/_deps/opensmt-src/regression"
+./server/smts.py  -o4 -l  -fp ${OPENSMTDIR}/QF_UF/NEQ004_size4.smt2,${OPENSMTDIR}/QF_LIA/can_solve/ex10100_2600_100.smt2,${OPENSMTDIR}/QF_UF/php_3_3_40_sat.smt2,${OPENSMTDIR}/QF_UF/php_3_3_40_unsat.smt2
 sleep 1
 
 
