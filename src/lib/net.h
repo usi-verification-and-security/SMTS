288c861b3e0ba99b43115fe5df0889495bce9c3c
Masoud Asadzadeh <masoud.asadzade@gmail.com>
2022-06-20 21:01:03 +0200

    [Skip Ci] LemmaServer: Rename capture to EventAndData and remove the class from net

--- src/lib/net.h
+++ src/lib/net.h
@@ -16,30 +16,4 @@
 #include "net/Server.h"
 #include "net/Socket.h"
 
-namespace PTPLib::common {
-    template<typename T, typename F>
-    class capture_move {
-        T x;
-        F f;
-    public:
-        capture_move(T && x, F && f)
-                : x{std::forward<T>(x)}, f{std::forward<F>(f)} {}
-
-        template<typename ...Ts>
-        auto operator()(Ts && ...args) -> decltype(f(x, std::forward<Ts>(args)...)) {
-            return f(x, std::forward<Ts>(args)...);
-        }
-
-        template<typename ...Ts>
-        auto operator()(Ts && ...args) const -> decltype(f(x, std::forward<Ts>(args)...)) {
-            return f(x, std::forward<Ts>(args)...);
-        }
-    };
-
-    template<typename T, typename F>
-    capture_move<T, F> capture(T && x, F && f) {
-        return capture_move<T, F>(std::forward<T>(x), std::forward<F>(f));
-    }
-}
-
 #endif
