5aed1ca9ed30628ccb10844e89888d17f4f58ad4
Masoud Asadzadeh <masoud.asadzade@gmail.com>
2022-06-22 15:50:04 +0200

    Circle CI: Pass thread and undefine sanitizers to the Debug/build/gcc and Debug/build/llvm

--- bin/run-server-client.sh
+++ bin/run-server-client.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 OPENSMTDIR=$(pwd)"/build/_deps/opensmt-src/regression"
-./server/smts.py  -o4 -l  -fp ${OPENSMTDIR}/QF_UF/NEQ004_size4.smt2,${OPENSMTDIR}/QF_LIA/can_solve/ex10100_2600_100.smt2,${OPENSMTDIR}/QF_UF/php_3_3_40_sat.smt2,${OPENSMTDIR}/QF_UF/php_3_3_40_unsat.smt2
+./server/smts.py -pn 3000 -p -pt 1 -o4 -l -fp ${OPENSMTDIR}/QF_UF/NEQ004_size4.smt2
+./server/smts.py -pn 3001 -p -pt 1 -o4 -l -fp ${OPENSMTDIR}/QF_LIA/can_solve/ex10100_2600_100.smt2
+./server/smts.py -pn 3002 -p -pt 1 -o4 -l -fp ${OPENSMTDIR}/QF_UF/php_3_3_40_sat.smt2
+./server/smts.py -pn 3003 -p -pt 1 -o4 -l -fp ${OPENSMTDIR}/QF_UF/php_3_3_40_unsat.smt2
 sleep 1
 
 
