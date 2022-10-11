e4d0cd39db4dbc59a5cbe7dadca562bb48be6669
Masoud Asadzadeh <masoud.asadzade@gmail.com>
2022-10-11 18:11:03 +0200

    [CI SKIP] version: Fix an error in which server could not lock config file ~/.gitconfig: File exists

--- server/version.py
+++ server/version.py
@@ -12,7 +12,7 @@ if sys.version_info.major != 3 or sys.version_info.minor < 5:
     sys.stderr.write('Python 3.5 is needed!\n')
     sys.exit(-1)
 
-version = int(subprocess.check_output([str(pathlib.Path(__file__).parent.parent / 'version.sh')]).strip())
+version = int(subprocess.check_output([str(pathlib.Path(__file__).parent.parent / 'version.sh'), '1']).strip())
 
 if __name__ == '__main__':
     print(version)
--- version.sh
+++ version.sh
@@ -2,5 +2,8 @@
 # -*- coding: utf-8 -*-
 
 cd "$(dirname "$0")"
-git config --global --add safe.directory /SMTS
+if [ $# -lt 1 ]
+then
+  git config --global --add safe.directory /SMTS
+fi
 git rev-list --count origin/cube-and-conquer | tr -d '\n'
