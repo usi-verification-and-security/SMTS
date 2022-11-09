48e22a6274f5f7a01b2126df3443eaecc7035285
Masoud Asadzadeh <masoud.asadzade@gmail.com>
2022-11-09 14:26:30 +0100

    [Skip Ci]temp-removable

--- version.sh
+++ version.sh
@@ -2,8 +2,5 @@
 # -*- coding: utf-8 -*-
 
 cd "$(dirname "$0")"
-if [ $# -lt 1 ]
-then
-  git config --global --add safe.directory /SMTS
-fi
+git config --global --add safe.directory /SMTS
 git rev-list --count origin/cube-and-conquer | tr -d '\n'
