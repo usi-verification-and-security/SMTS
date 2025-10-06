11fd7507a732a872b44db0c81564cb002265abaf
Tomáš Kolárik <tomaqa@gmail.com>
2025-10-06 09:46:50 +0200

    experiment scripts for plotting

---
+++ experiments/scripts/cactus_plot.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+results=("$@")
+
+# [[ -z $TIMEOUT ]] && TIMEOUT=300
+[[ -z $TIMEOUT ]] && TIMEOUT=1200
+
+IMAGE_FILE=plot.png
+GNUPLOT_FILE=plot.gp
+
+function cleanup {
+  rm -f $GNUPLOT_FILE
+}
+
+python make_cactus_plot_gp.py -o "$IMAGE_FILE" -t $TIMEOUT "${results[@]}" >"$GNUPLOT_FILE" || {
+    status=$?
+    [[ -s $GNUPLOT_FILE ]] && less "$GNUPLOT_FILE"
+    cleanup
+    exit $status
+}
+
+gnuplot "$GNUPLOT_FILE" || exit $?
+
+# printf "The resulting plot is stored in %s\n" "$IMAGE_FILE"
+
+display "$IMAGE_FILE"
+
+cleanup
+exit 0
