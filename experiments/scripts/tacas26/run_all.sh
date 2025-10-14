0871595897a7dd497969caaf4da55f3af2685026
Tomáš Kolárik <tomaqa@gmail.com>
2025-10-14 10:45:35 +0200

    used ranges for splitting experiment input files into chunks, and using local/

--- experiments/scripts/tacas26/run_all.sh
+++ experiments/scripts/tacas26/run_all.sh
@@ -3,6 +3,7 @@
 TACAS_SCRIPTS_DIR=$(dirname $(realpath "$0"))
 SCRIPTS_DIR=$(realpath "$TACAS_SCRIPTS_DIR/../")
 ROOT_DIR=$(realpath "$SCRIPTS_DIR/../../")
+LOCAL_DATA_DIR=$(realpath "$ROOT_DIR/local")
 
 if [[ -z $OLD_ALG ]]; then
   VERSION=final-alg
@@ -16,81 +17,44 @@ fi
 
 export TIMEOUT=1200
 
+## Due to limited job time limit (e.g. 24 h), we need to split the task into subtasks
+CHUNK_SIZE=100
+
 FILES=(
-  "$ROOT_DIR/data/QF_LRA_files_1-250"
-  "$ROOT_DIR/data/QF_LRA_files_251-500"
-  "$ROOT_DIR/data/QF_LRA_files_501-750"
-  "$ROOT_DIR/data/QF_LRA_files_751-1000"
-  "$ROOT_DIR/data/QF_LRA_files_1001-1250"
-  "$ROOT_DIR/data/QF_LRA_files_1251-1500"
-  "$ROOT_DIR/data/QF_LRA_files_1501-1753"
-  "$ROOT_DIR/data/QF_LIA_files_1-250"
-  "$ROOT_DIR/data/QF_LIA_files_251-500"
-  "$ROOT_DIR/data/QF_LIA_files_501-750"
-  "$ROOT_DIR/data/QF_LIA_files_751-1000"
-  "$ROOT_DIR/data/QF_LIA_files_1001-1250"
-  "$ROOT_DIR/data/QF_LIA_files_1251-1500"
-  "$ROOT_DIR/data/QF_LIA_files_1501-1750"
-  "$ROOT_DIR/data/QF_LIA_files_1751-2000"
-  "$ROOT_DIR/data/QF_LIA_files_2001-2250"
-  "$ROOT_DIR/data/QF_LIA_files_2251-2500"
-  "$ROOT_DIR/data/QF_LIA_files_2501-2750"
-  "$ROOT_DIR/data/QF_LIA_files_2751-3000"
-  "$ROOT_DIR/data/QF_LIA_files_3001-3250"
-  "$ROOT_DIR/data/QF_LIA_files_3251-3500"
-  "$ROOT_DIR/data/QF_LIA_files_3501-3750"
-  "$ROOT_DIR/data/QF_LIA_files_3751-4000"
-  "$ROOT_DIR/data/QF_LIA_files_4001-4250"
-  "$ROOT_DIR/data/QF_LIA_files_4251-4500"
-  "$ROOT_DIR/data/QF_LIA_files_4501-4750"
-  "$ROOT_DIR/data/QF_LIA_files_4751-5000"
-  "$ROOT_DIR/data/QF_LIA_files_5001-5250"
-  "$ROOT_DIR/data/QF_LIA_files_5251-5500"
-  "$ROOT_DIR/data/QF_LIA_files_5501-5750"
-  "$ROOT_DIR/data/QF_LIA_files_5751-6000"
-  "$ROOT_DIR/data/QF_LIA_files_6001-6250"
-  "$ROOT_DIR/data/QF_LIA_files_6251-6500"
-  "$ROOT_DIR/data/QF_LIA_files_6501-6750"
-  "$ROOT_DIR/data/QF_LIA_files_6751-7000"
-  "$ROOT_DIR/data/QF_LIA_files_7001-7250"
-  "$ROOT_DIR/data/QF_LIA_files_7251-7500"
-  "$ROOT_DIR/data/QF_LIA_files_7501-7750"
-  "$ROOT_DIR/data/QF_LIA_files_7751-8000"
-  "$ROOT_DIR/data/QF_LIA_files_8001-8250"
-  "$ROOT_DIR/data/QF_LIA_files_8251-8500"
-  "$ROOT_DIR/data/QF_LIA_files_8501-8750"
-  "$ROOT_DIR/data/QF_LIA_files_8751-9000"
-  "$ROOT_DIR/data/QF_LIA_files_9001-9250"
-  "$ROOT_DIR/data/QF_LIA_files_9251-9500"
-  "$ROOT_DIR/data/QF_LIA_files_9501-9750"
-  "$ROOT_DIR/data/QF_LIA_files_9751-10000"
-  "$ROOT_DIR/data/QF_LIA_files_10001-10250"
-  "$ROOT_DIR/data/QF_LIA_files_10251-10500"
-  "$ROOT_DIR/data/QF_LIA_files_10501-10750"
-  "$ROOT_DIR/data/QF_LIA_files_10751-11000"
-  "$ROOT_DIR/data/QF_LIA_files_11001-11250"
-  "$ROOT_DIR/data/QF_LIA_files_11251-11500"
-  "$ROOT_DIR/data/QF_LIA_files_11501-11750"
-  "$ROOT_DIR/data/QF_LIA_files_11751-12000"
-  "$ROOT_DIR/data/QF_LIA_files_12001-12250"
-  "$ROOT_DIR/data/QF_LIA_files_12251-12500"
-  "$ROOT_DIR/data/QF_LIA_files_12501-12750"
-  "$ROOT_DIR/data/QF_LIA_files_12751-13000"
-  "$ROOT_DIR/data/QF_LIA_files_13001-13224"
+  "$LOCAL_DATA_DIR/QF_LRA_files"
+  "$LOCAL_DATA_DIR/QF_LIA_files"
 )
 
 for f in "${FILES[@]}"; do
-  for o in $os; do
-    if (( $o < 8 )); then
-      ps=1
-      ls=1
+  [[ -r $f ]] || {
+    printf 'File "%s" is not readable.\n' "$f" >&2
+    exit 2
+  }
+
+  wcl=$(wc -l <"$f")
+  for (( lo = 1; lo <= $wcl; lo += $CHUNK_SIZE )); do
+    hi=$(( lo + $CHUNK_SIZE - 1 ))
+    (( hi > $wcl )) && hi=$wcl
+    if (( lo == 1 && hi == $wcl )); then
+      lo_arg=
+      hi_arg=
     else
-      ps='1 0'
-      ls='1 0'
+      lo_arg=$lo
+      hi_arg=$hi
     fi
-    for p in $ps; do
-      for l in $ls; do
-        PARTITIONING=$p LEMMA_SHARING=$l os=$o "$TACAS_SCRIPTS_DIR/sbatch.sh" $VERSION "$f"
+
+    for o in $os; do
+      if (( $o < 8 )); then
+        ps=1
+        ls=1
+      else
+        ps='1 0'
+        ls='1 0'
+      fi
+      for p in $ps; do
+        for l in $ls; do
+          PARTITIONING=$p LEMMA_SHARING=$l os=$o "$TACAS_SCRIPTS_DIR/sbatch.sh" $VERSION "$f" $lo_arg $hi_arg
+        done
       done
     done
   done
